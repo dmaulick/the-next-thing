3a999c8d1a802a48b3aae9df6ffa5daeafbccf2d
David Maulick <davidmaulick@gmail.com>
2022-04-17 13:54:28 -0500

    create nx workspace for a React App

---
+++ the-next-thing/apps/the-react-web-thing/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'the-react-web-thing',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/the-react-web-thing',
+};
